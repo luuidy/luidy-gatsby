e5e6dbb87078929293a84cc260f09de5ca9187dc
luuidy <luidy.informatica@gmail.com>
2021-07-31 17:41:34 -0300

    homepage | estilizando

--- src/components/CardCategorie.js
+++ src/components/CardCategorie.js
@@ -4,7 +4,8 @@ import '../styles/components/cardCategorie.scss'
 
 export default function CardCategorie() {
     return (
-        <div className="card">
+        <div className="card" style={{backgroundImage: "url('https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_960_720.jpg')"}}>
+            <div className="card__mask"/>
             <div className="card__content">
                 <h1>Camisas</h1>
             </div>
