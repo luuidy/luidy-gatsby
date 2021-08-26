1c6de24cea8c3d8982ddeb4e0c127c7ada1fde0b
luuidy <luidy.informatica@gmail.com>
2021-08-26 20:04:03 -0300

    login pt.1

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'gatsby'
 import icon from '../images/diamond_white.png'
 
 import "../styles/login.scss"
-const login = () => {
+const Login = () => {
+
+
     return (
         <div className="login">
             <header className="login__header">
                 <Link to="/"><img src={icon} alt="icon"/></Link>
             </header>
-            <h1>Login</h1>
+            <section className="container">
+                <div className="entrar">
+                    <div className="entrar__header">
+                        <h1>Entrar</h1>
+                        <span>Entre com Login e Senha</span>
+                    </div>
+                    <form className="form">
+                        <div className="form__group">
+                            <input 
+                                className="form__input" 
+                                name="email" 
+                                type="email" />
+                            <label htmlFor="email" className="form__label">
+                                Email
+                            </label>
+                        </div>
+                        <div className="form__group">
+                            <input className="form__input" name="senha"  required type="password" />
+                            <label htmlFor="senha" className="form__label">
+                                Senha
+                            </label>
+                        </div>
+
+                        <div className="form__buttons">
+
+                        </div>
+                    </form>
+                </div>
+                <div className="cadastrar">
+                    Cadastrar
+                </div>
+            </section>
         </div>
     )
 }
 
-export default login
+export default Login
