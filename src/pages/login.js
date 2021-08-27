f325475e9dd26387b16d9f02c10411056c292609
luuidy <luidy.informatica@gmail.com>
2021-08-26 21:23:46 -0300

    usando placeholder

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,14 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
+import { useForm } from "react-hook-form";
 import {Link} from 'gatsby'
 import icon from '../images/diamond_white.png'
 
 import "../styles/login.scss"
 const Login = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
 
+    console.log(watch("email")); // watch input value by passing the name of it
 
     return (
         <div className="login">
@@ -17,25 +21,34 @@ const Login = () => {
                         <h1>Entrar</h1>
                         <span>Entre com Login e Senha</span>
                     </div>
-                    <form className="form">
+                    <form className="form" onSubmit={handleSubmit(onSubmit)}>
                         <div className="form__group">
                             <input 
-                                className="form__input" 
+                                className={`form__input`}
                                 name="email" 
-                                type="email" />
+                                type="email"
+                                placeholder=" "
+                                defaultValue="" {...register("email")}
+                                />
                             <label htmlFor="email" className="form__label">
                                 Email
                             </label>
                         </div>
                         <div className="form__group">
-                            <input className="form__input" name="senha"  required type="password" />
+                            <input 
+                                className="form__input" 
+                                name="senha" 
+                                type="password"
+                                placeholder=" "
+                                defaultValue="" {...register("senha")}
+                                />
                             <label htmlFor="senha" className="form__label">
                                 Senha
                             </label>
                         </div>
 
                         <div className="form__buttons">
-
+                            <button type="submit">Entrar</button>
                         </div>
                     </form>
                 </div>
